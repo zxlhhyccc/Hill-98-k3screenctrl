76b492cc131344cfd1484f1b2eef1e81b849bd33
zxlhhyccc <zxlhhy@gmail.com>
2019-05-11 12:54:47 +0800

    fix basic.sh and device_online,sh

--- lib/k3screenctrl/basic.sh
+++ lib/k3screenctrl/basic.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 . /etc/os-release
+. /etc/openwrt_release
 
 PRODUCT_NAME_FULL=$(cat /etc/board.json | jsonfilter -e "@.model.name")
 PRODUCT_NAME=${PRODUCT_NAME_FULL#* } # Remove first word to save space
@@ -9,11 +10,17 @@ MAC_ADDR=$(ifconfig $WAN_IFNAME | grep -oE "([0-9A-Z]{2}:){5}[0-9A-Z]{2}")
 
 CPU_TEMP=$(($(cat /sys/class/thermal/thermal_zone0/temp) / 1000))
 
-HW_VERSION=${LEDE_DEVICE_REVISION:0:2}
-FW_VERSION=${BUILD_ID:0:17}
+HW_VERSION_CACHE_FILE=/tmp/hw_version
+if [ -e "$HW_VERSION_CACHE_FILE" ]; then
+    HW_VERSION=$(cat $HW_VERSION_CACHE_FILE)
+else
+    HW_VERSION=$(nvram get hd_version)
+    echo $HW_VERSION > $HW_VERSION_CACHE_FILE
+fi
+
+FW_VERSION=${DISTRIB_REVISION:0:17}
 
 echo $PRODUCT_NAME
-
 if [ $(uci get k3screenctrl.@general[0].cputemp) -eq 1 ]; then
     echo $HW_VERSION $CPU_TEMP
 else
--- lib/k3screenctrl/device_online.sh
+++ lib/k3screenctrl/device_online.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 file=/tmp/k3screenctrl/device_online
 if [ $# -eq 0 ]; then
-    pid=$(pidof $(basename $0) | sed 's/'$$'//')
-    kill -9 $pid >/dev/null 2>&1
-    # echo $$ > $pid_file
-    # device_list=$(grep -w br-lan /proc/net/arp | grep -w 192.168 | awk '{print $1}')
-    device_list=$(cat $(uci get dhcp.@dnsmasq[0].leasefile) | awk '{print $3}')
+    device_list=$(cat $(uci get dhcp.@dnsmasq[0].leasefile) | grep $(uci get network.lan.ipaddr | awk -F . '{print $1"."$2"."$3"."}') | awk '{print $3}')
     [ -s $file ] || {
         for device in $device_list
         do
@@ -16,12 +12,9 @@ if [ $# -eq 0 ]; then
     do
         $0 $device >/dev/null 2>&1 &
     done
-    sleep 60
-    $0 &
-    # rm $pid_file
 else
     device=$1
-    arping -I br-lan -f -w 1 -q $device
+    arping -f -q -w 3 -I br-lan $device
     c=$?
     i="$(grep $device $file)"
     if [ -z "$i" ]; then
